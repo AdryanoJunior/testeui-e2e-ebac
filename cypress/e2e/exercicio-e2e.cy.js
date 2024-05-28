8f2710bea8e20e2f83cdfa012886fa9508084c82
Adryano Junior <jusamsung10@gmail.com>
2024-05-28 16:18:16 -0300

    Teste de ponta a ponta no Ecommerce

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,49 @@
 /// <reference types="cypress" />
+const login = require('../fixtures/perfil.json')
+import produtosPage from '../support/page_objects/produtos.page';
+import { faker } from '@faker-js/faker';
 
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
+describe('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
+ 
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
-
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
-
-
-})
+    beforeEach(() => {
+        produtosPage.visitarUrl()
+     });
+ 
+     afterEach(() => {
+       cy.screenshot()
+     });
+ 
+   it('Deve fazer login com sucesso - Usando Fixture', () => {
+       cy.loginFixture()
+       cy.get('.woocommerce-form > .button').click()
+       cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
+       
+   });
+ 
+     });
+ 
+   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+   produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
+   cy.addProdutoCarrinho()
+   cy.get('.woocommerce-message').should('contain', '2 × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+   cy.get('.woocommerce-message').should('contain', '2 × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+ 
+   cy.verCarrinho()
+ 
+   cy.dadosPessoais(faker.person.firstName(),faker.person.lastName(),faker.location.streetAddress(),faker.location.city(),'01521-020',faker.phone.imei(),faker.internet.email() )
+   cy.get('.woocommerce-form-coupon-toggle > .woocommerce-info').should('contain','Você tem um cupom de desconto? Clique aqui e informe o código do seu cupom de desconto')
+   
+   cy.finalizarCompra()
+   cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+ 
+ });
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,3 +4,43 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('addProdutoCarrinho', () =>{
+    cy.get('.button-variable-item-M').click()
+    cy.get('.button-variable-item-Yellow').click()
+    cy.get('.plus').click()
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.button-variable-item-L').click()
+    cy.get('.button-variable-item-Brown').click()
+    cy.get('.single_add_to_cart_button').click()   
+})
+
+Cypress.Commands.add('verCarrinho', () => {
+    cy.get('.woocommerce-message > .button').click()
+    cy.get('h2').should('contain', 'Total no carrinho')
+    cy.get('.checkout-button').click()   
+})
+
+Cypress.Commands.add('dadosPessoais', (nome, sobrenome, endereço, cidade, CEP, telefone, email) => {
+    cy.get('#billing_first_name').type(nome)
+    cy.get('#billing_last_name').type(sobrenome)
+    cy.get('#billing_address_1').type(endereço)
+    cy.get('#billing_city').type(cidade)
+    cy.get('#billing_postcode').type(CEP)
+    cy.get('#billing_phone').type(telefone)
+    cy.get('#billing_email').type(email)
+})
+
+Cypress.Commands.add('finalizarCompra', () => {
+    cy.get('#payment_method_bacs').click()
+    cy.get('#terms').click()
+    cy.get('#place_order').click()  
+})
+
+Cypress.Commands.add('loginFixture', () =>{
+    cy.fixture('perfil').then( dados =>{
+        cy.get('#username').type(dados.usuario)
+        cy.get('#password').type(dados.senha)
+        cy.get('.woocommerce-form > .button').click()
+})
+})
+
